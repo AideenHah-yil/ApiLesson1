f0eabb34ceac2bcc63a474b045895b5092584742
Ruth Wangare(AideenChayil) <160226360+AideenHah-yil@users.noreply.github.com>
2024-04-26 10:05:02 +0000

    Initial commit

---
+++ django_project/views.py
@@ -0,0 +1,17 @@
+import requests
+from django.shortcuts import render
+
+def home(request):
+#USING APIS -Example 1
+  #response,data and result is a variable
+  response = requests.get('https://api.github.com/events')
+  data = response.json()
+  result = data[0]["repo"]
+
+  #example 2
+  response2 = requests.get('https://dog.ceo/api/breeds/image/random')
+  data2 = response2.json()
+  result2 = data2["message"]
+  
+
+  return render(request, 'templates/index.html', {'result': result, 'result2': result2})
